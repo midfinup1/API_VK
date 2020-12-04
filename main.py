a41ad7c21a5fe8a7ce4306fdd3a4b7aef45e1631
Kiselev Edward <steammfu@yandex.ru>
2020-12-04 11:28:13 +0700

    FInal

--- main.py
+++ main.py
@@ -2,57 +2,62 @@ import requests
 
 
 class User:
-    def __init__(self, user_id, domain):
-        self.id = user_id
-        self.domain = domain
-        # self.id = input('Введите id пользователя: ')
 
-    def search_mutual_friends(self, source_uid, target_uid):
-        VK_TOKEN = input('Введите токен ВК: ')
-        API_BASE_URL = 'https://api.vk.com/method/'
-        V = '5.126'
-        source_uid = source_user.id
-        target_uid = target_user.id
-        # self.source_uid = input('Введите id первого пользователя: ')
-        # self.target_uid = input('Введите id второго пользователя: ')
-        request_url = (API_BASE_URL + 'friends.get')
-        response = requests.get(request_url, params={
-            'access_token': VK_TOKEN,
-            'v': V,
-            'user_id': source_uid,
-            'fields': 'domain',
-            'order': 'name'
-        })
-        friend_list = {}
-        count = response.json()['response']['count']
-        for i in range(0, count):
-            for friend in response.json()['response']['items']:
-                friend_list[friend['id']] = 'https://vk.com/' + friend['domain']
-        response = requests.get(request_url, params={
-            'access_token': VK_TOKEN,
-            'v': V,
-            'user_id': target_uid,
-            'fields': 'domain',
-            'order': 'name'
-        })
-        mutual_friends = []
-        response = response.json()['response']['items']
-        temp_list = []
-        for i in response:
-            temp_list.append(i['id'])
-        for friend in friend_list.items():
-            if friend[0] in temp_list:
-                user = 'user' + str(friend[0])
-                mutual_friends.append(user)
-                user = User([friend[0]], friend[1])
+    def __init__(self, token, user_id=''):
+        self.token = token
+        self.user_id = user_id
 
-        print(mutual_friends)
+    def __and__(self, other):
+        resp1 = self.get_friends()
+        resp2 = other.get_friends()
+        common = []
+        for item in resp1['response']['items']:
+            if item in resp2['response']['items']:
+                common.append(item)
+        return common
 
+    def get_status(self):
+        response = requests.get(
+            'https://api.vk.com/method/status.get',
+            params={
+                'access_token': self.token,
+                'v': 5.103
+            }
+        )
+        return response.json()
 
-source_user = User(196055093, '')
-target_user = User(237127625, '')
-source_user.search_mutual_friends(source_user, target_user)
-# print(user.domain)
+    def set_status(self, text):
+        response = requests.get(
+            'https://api.vk.com/method/status.set',
+            params={
+                'access_token': self.token,
+                'v': 5.103,
+                'text': text
+            }
+        )
+        return response.json()
+
+    def __repr__(self):
+        return 'https://vk.com/id' + str(self.user_id)
+
+    def get_friends(self):
+        response = requests.get(
+            'https://api.vk.com/method/friends.get',
+            params={
+                'access_token': self.token,
+                'v': 5.103,
+                'user_id': self.user_id
+            }
+        )
+        return response.json()
+
+TOKEN = '10b2e6b1a90a01875cfaa0d2dd307b7a73a15ceb1acf0c0f2a9e9c586f3b597815652e5c28ed8a1baf13c'
+Anna = User(TOKEN, '196055093')
+Petya = User(TOKEN, '237127625')
+print(Anna & Petya)
+print(Anna)
+print(Petya)
+
 
 # 196055093
 # 237127625
